e478724974e9cfd28626e3aab766ee94995a11a9
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-08 15:48:01 +0100

    added funcionality for burger-navigation

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,5 @@
-// import './sass/base.scss';
-// import './sass/colors.scss';
-// import './sass/mixins.scss';
-// import './sass/feature-plan.scss';
-// import './sass/form.scss';
-// import './sass/header.scss';
-// import './sass/nav.scss';
-// import './sass/footer.scss';
-
-
-// czy tutaj nie powinno być 
-
 import './sass/main.scss'
+
+import {
+    handleNav
+} from './js/handleNav';
