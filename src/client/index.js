806f94cfbdfa243c031a57f514003c6d092be162
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-08 15:25:30 +0100

    added Sass styles , error with webpack

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,11 @@
 import './styles/base.scss';
+import './styles/colors.scss';
+import './styles/mixins.scss';
+
+
+
+import './styles/feature-plan.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/nav.scss';
+import './styles/footer.scss';
