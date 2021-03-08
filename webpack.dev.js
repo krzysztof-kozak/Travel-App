fb549625cd0987134d3e6e877acdcfdbb49635e7
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-08 14:56:40 +0100

    set up webpack, webpack loaders and plugins

---
+++ webpack.dev.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+    entry: "./src/client/index.js", // our entry file 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['@babel/preset-env', {
+                            targets: "defaults"
+                        }]
+                    ]
+                }
+            }
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', "sass-loader"],
+        }, ]
+    },
+    plugins: [
+        new WorkboxPlugin.GenerateSW(),
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        // new CopyPlugin({ //if we use images
+        //     patterns: [{
+        //         from: "",
+        //         to: "./img"
+        //     }, ],
+        // }),
+    ]
+
+}
