453fa239ae28be1e52ed93131c8a3783c60c53c8
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-11 23:07:09 +0100

    test : error in app.test.js

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,11 +1,17 @@
-import {
-    axios
-} from '../src/client/js/app';
+const getDataFromApi = require('../src/client/js/app');
+const axios = require('axios');
 
+jest.mock('axios');
 
+it('returns the title of the first album', async () => {
+    axios.get.mockResolvedValue({
+        data: [{
+            userId: 1,
+            id: 1,
+            title: 'My First Album'
+        }, ]
+    });
 
-describe('check if axios defined', () => {
-    test('check if axios defined', () => {
-        expect(axios).toBeDefined();
-    })
-})
+    const title = await getDataFromApi();
+    expect(title).toEqual('My First Album');
+});
