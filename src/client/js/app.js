f8400ea9d2dfef319392af0cba08c2115130e098
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-17 12:58:40 +0100

    deleted // Api keys for submission purpose

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -24,12 +24,6 @@ const enterCity = document.querySelector('.city')
 const weatherDescription = document.querySelector('.weather')
 const imgCountry = document.querySelector('.feature-plan__img-city')
 
-// Api keys for submission purpose
-const geonamesUsername = 'karolina'
-const pixabayApiKey = '20598717-3c4f9262e5a960e1db9ad6571'
-const weatherbitApiKey = '72fa43f538bb4b0eb570bfa7cfc92a4b'
-
-
 
 // API geonames api
 //what we need latitude, longitude, country
@@ -197,7 +191,6 @@ btnDelete.addEventListener('click', cleanUp)
 
 
 export {
-    start,
     getDataFromApi,
     appUpDate,
     setTime,
